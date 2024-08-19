a5079db6b9589b0d22e6465a217761d6e03f617e
Felipe <felipelarumbe157@gmail.com>
2024-08-19 10:05:04 -0300

    Hecho

---
+++ script.js
@@ -0,0 +1,45 @@
+function findTheOldest(people){
+  const oldest = people.reduce((oldestPerson, currentPerson) => {
+    
+      const currentAge = (currentPerson.yearOfDeath || new Date().getFullYear()) - currentPerson.yearOfBirth;
+
+      const oldestAge = (oldestPerson.yearOfDeath || new Date().getFullYear()) - oldestPerson.yearOfBirth;
+
+      return currentAge > oldestAge ? currentPerson : oldestPerson;
+        });
+
+        console.log(oldest);
+        return oldest;
+
+
+}
+
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+    {
+      name: "Bob",
+      yearOfBirth: 1950,
+      yearOfDeath: 2023,
+    },
+    {
+      name: "Ana",
+      yearOfBirth: 1905,
+      yearOfDeath: 1990,
+    }
+  ]
+
+  findTheOldest(people);
